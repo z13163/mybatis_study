88cc69dae1e829df0d574390273d6873ae38b259
z13163 <1316385513@qq.com>
2021-09-08 17:08:54 +0800

    mybatis-study

---
+++ mybatis-06/src/main/java/utils/MybatisUtils.java
@@ -0,0 +1,30 @@
+package utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MybatisUtils {
+    //获取对象SqlSessionFactory
+
+    private static SqlSessionFactory sqlSessionFactory;
+    static {
+        try {
+                String resource = "mybatis-config.xml";
+                InputStream inputStream = Resources.getResourceAsStream(resource);
+                sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSession sqlSession(){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        return sqlSession;
+    }
+
+}
